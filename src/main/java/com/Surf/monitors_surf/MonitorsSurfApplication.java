abec333dc05de1314a1912d7a64de8cb37e2e5a9
Pacheca <mpacheca7@gmail.com>
2025-05-26 11:11:06 +0200

    config

---
+++ src/main/java/com/Surf/monitors_surf/MonitorsSurfApplication.java
@@ -0,0 +1,13 @@
+package com.Surf.monitors_surf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorsSurfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorsSurfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Surf/monitors_surf/MonitorsSurfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Surf.monitors_surf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorsSurfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
